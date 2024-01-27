a46769444d28d58de22ab150c848aec99224187d
yacin <yacin.ha9@gmail.com>
2024-01-27 17:55:16 +0100

    implement the backpropagation and update the weights

--- udacity_dl/first-neural-network/my_answers.py
+++ udacity_dl/first-neural-network/my_answers.py
@@ -91,17 +91,17 @@ class NeuralNetwork(object):
         error = y - final_outputs # Output layer error is the difference between desired target and actual output.
         
         # TODO: Calculate the hidden layer's contribution to the error
-        hidden_error = None
+        output_error_term = error * final_outputs * (1 - final_outputs)
         
         # TODO: Backpropagated error terms - Replace these values with your calculations.
-        output_error_term = None
+        hidden_error = np.dot(output_error_term, self.weights_hidden_to_output)
         
-        hidden_error_term = None
+        hidden_error_term = hidden_error_term * hidden_outputs * (1 - hidden_outputs)
         
         # Weight step (input to hidden)
-        delta_weights_i_h += None
+        delta_weights_i_h += hidden_error_term, X[:, None])
         # Weight step (hidden to output)
-        delta_weights_h_o += None
+        delta_weights_h_o += output_error_term * hidden_outputs
         return delta_weights_i_h, delta_weights_h_o
 
     def update_weights(self, delta_weights_i_h, delta_weights_h_o, n_records):
